c97b206b67f693249640a8edd3609287fd1a4b6f
Julie Branyan <juliebranyan@gmail.com>
2021-08-24 06:55:05 -0700

    Create random phrase, start button functionality

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,48 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
- * Game.js */
+ * Game.js 
+ * Game.js to create a Game class methods for starting and ending the game, handling
+interactions, getting a random phrase, checking for a win, and removing a life from the
+scoreboard.*/
+
+class Game {
+    constructor(){
+        this.missed = 0;
+        this.phrases = this.createPhrases();
+        this.activePhrases = null;
+    }
+/**
+ * Creates phrases for use in the game
+ * @return An array of phrases that could be used in the game
+ */
+    createPhrases() {
+        const phrases = [
+            new Phrase('May your coffee be strong'),
+            new Phrase('Life is like a box of chocolates'),
+            new Phrase('More Javascript'),
+            new Phrase('Eat more food'),
+            new Phrase('You are cool')];
+        
+            return phrases;
+    }
+
+
+        /**
+     * Selects random phrase from phrases property
+     * @return Phrase object chosen to be used
+     */
+    
+         getRandomPhrase(){
+            return this.phrases[Math.floor(Math.random() * this.phrases.length)];
+        }
+
+    /**
+    * Begins game by selecting a random phrase and displaying it to user
+    */
+        startGame(){
+            const hideOverlay = document.getElementById('overlay').style.display = 'none';
+            this.activePhrase = this.getRandomPhrase();
+            this.activePhrase.addPhraseToDisplay();
+
+        }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,59 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
- * Phrase.js */
+ * Phrase.js 
+ * Phrase.js to create a Phrase class to handle the creation of phrases.*/
+
+class Phrase{
+    constructor(phrase){
+        this.phrase = phrase.toLowerCase();
+    }
+    /**
+     * This method adds letter placeholders to the display when the game starts. 
+     * 
+     */
+    
+    addPhraseToDisplay(){
+        //Get ul element for the class with the id "phrase"
+        const phraseFirstElement = document.getElementById('phrase').firstElementChild;
+        const phrase = this.phrase;
+
+
+        //In the for loop, grab the first letter of the phrase and create a seperate li element for each letter
+        // next steps add class name to letter and space using example phrase html
+        for(let i=0; i < this.phrase.length; i++ ){
+            const liCharacterElement = document.createElement('LI');
+            const letter = this.phrase.charAt(i);
+            liCharacterElement.innerHTML = letter;
+            
+            if(letter !== ' '){
+                liCharacterElement.className = `hide letter ${liCharacterElement.innerHTML}`;
+            } else {
+                liCharacterElement.className = `space ${liCharacterElement.innerHTML}`}
+                phraseFirstElement.appendChild(liCharacterElement);
+            }
+
+
+    }
+    
+
+    /**
+     * checks to see if the letter selected by the player matches a letter in the phrase.
+     */
+    checkLetter(){
+    }
+
+
+    /**
+     * reveals the letter(s) on the board that matches the player's selection.To reveal the 
+     * matching letter(s), select all of the letter DOM elements that have a CSS class 
+     * name that matches the selected letter and replace each selected element's hide 
+     * CSS class with the show 
+     */
+
+    showMatchedLetter(){
+        document.querySelectorAll();
+
+    }
+
+
+}
